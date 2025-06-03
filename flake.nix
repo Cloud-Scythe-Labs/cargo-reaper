6396f7f35dd7d26cb836a2b64c8840d93e38e438
Chris O'Brien <obrchr96@gmail.com>
2025-06-03 01:11:09 -0700

    chore: Add test suite

--- flake.nix
+++ flake.nix
@@ -27,125 +27,148 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      crane,
-      fenix,
-      nix-core,
-      flake-utils,
-      advisory-db,
-      ...
+    { self
+    , nixpkgs
+    , crane
+    , fenix
+    , nix-core
+    , flake-utils
+    , advisory-db
+    , ...
     }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (pkgs) lib;
+      inherit (pkgs) lib;
 
-        rustToolchain = nix-core.toolchains.${system}.mkRustToolchainFromTOML
-          ./.rust-toolchain.toml
-          "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain.fenix-pkgs;
-        src = craneLib.cleanCargoSource ./.;
+      rustToolchain = nix-core.toolchains.${system}.mkRustToolchainFromTOML
+        ./.rust-toolchain.toml
+        "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain.fenix-pkgs;
+      src = craneLib.cleanCargoSource ./.;
 
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
 
-          buildInputs = [
-            rustToolchain.darwin-pkgs
-          ];
-        };
+        buildInputs = [
+          rustToolchain.darwin-pkgs
+        ];
+      };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        cargo-reaper-drv = craneLib.buildPackage (commonArgs // {
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      cargo-reaper-drv = craneLib.buildPackage (commonArgs // {
+        inherit cargoArtifacts;
+        doCheck = false;
+      });
+    in
+    {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        inherit cargo-reaper-drv;
+
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        cargo-clippy = craneLib.cargoClippy (commonArgs // {
           inherit cargoArtifacts;
-          doCheck = false;
+          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
         });
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit cargo-reaper-drv;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          cargo-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+        cargo-doc = craneLib.cargoDoc (commonArgs // {
+          inherit cargoArtifacts;
+        });
 
-          cargo-doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-          });
-
-          # Check formatting
-          cargo-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          taplo-fmt = craneLib.taploFmt {
-            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-          };
-
-          # Audit dependencies
-          cargo-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-
-          # Audit licenses
-          cargo-deny = craneLib.cargoDeny {
-            inherit src;
-          };
-
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          cargo-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-            cargoNextestPartitionsExtraArgs = "--no-tests=warn";
-          });
+        # Check formatting
+        cargo-fmt = craneLib.cargoFmt {
+          inherit src;
         };
 
-        packages = rec {
-          cargo-reaper = cargo-reaper-drv;
-          default = cargo-reaper;
+        taplo-fmt = craneLib.taploFmt {
+          src = lib.sources.sourceFilesBySuffices src [ ".toml" ];
         };
 
-        apps = rec {
-          cargo-reaper = flake-utils.lib.mkApp {
+        # Audit dependencies
+        cargo-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
+
+        # Audit licenses
+        cargo-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+
+        # Run tests with cargo-nextest
+        cargo-nextest = craneLib.cargoNextest (commonArgs // {
+          inherit cargoArtifacts;
+          partitions = 1;
+          partitionType = "count";
+          cargoNextestPartitionsExtraArgs = "--no-tests=warn";
+        });
+      } // lib.optionalAttrs pkgs.stdenv.isLinux {
+        test-cargo-reaper-new =
+          let
+            tests = import ./tests {
+              inherit pkgs;
+              imports = [
+                {
+                  networking.useDHCP = true;
+                  environment.systemPackages = [
+                    self.packages.${system}.default
+                  ];
+                }
+              ];
+            };
+          in
+          pkgs.nixosTest {
+            name = "test-cargo-reaper-new";
+            nodes = {
+              inherit (tests) machine;
+            };
+            testScript = tests.testCargoReaperNew;
+          };
+      };
+
+      packages = rec {
+        cargo-reaper = cargo-reaper-drv;
+        default = cargo-reaper;
+      };
+
+      apps = rec {
+        cargo-reaper = flake-utils.lib.mkApp
+          {
             drv = cargo-reaper-drv;
           } // {
-            meta = {
-              homepage = "https://github.com/Cloud-Scythe-Labs/cargo-reaper/";
-              description = "A Cargo plugin for developing REAPER extension plugins with Rust.";
-              license = lib.licenses.mit;
-              maintainers = with lib.maintainers; [ eureka-cpu ];
-            };
+          meta = {
+            homepage = "https://github.com/Cloud-Scythe-Labs/cargo-reaper/";
+            description = "A Cargo plugin for developing REAPER extension plugins with Rust.";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ eureka-cpu ];
           };
-          default = cargo-reaper;
         };
+        default = cargo-reaper;
+      };
 
-        devShells.default = craneLib.devShell {
-          checks = self.checks.${system};
-          packages = [
-            pkgs.mdbook
-            self.packages.${system}.default
-          ];
-        };
-      }
-    );
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
+        packages = with pkgs; [
+          nil
+          nixpkgs-fmt
+          mdbook
+          self.packages.${system}.default
+        ];
+      };
+
+      formatter = pkgs.nixpkgs-fmt;
+    });
 }
---
+++ tests/default.nix
@@ -0,0 +1,69 @@
+{ pkgs, imports, ... }:
+let
+  # REAPER doesn't like to execute in the background, and NixOS Test invokes commands
+  # as root. To combat this, we change users based on machine before launching REAPER,
+  # using xvfb and xdotool to search for an error window before exiting successfully.
+  mkReaperDryRun =
+    { user
+    , reaper ? pkgs.reaper
+    , xdotool ? pkgs.xdotool
+    , xvfb-run ? pkgs.xvfb-run
+    }:
+    pkgs.writeShellScriptBin "reaper_dry_run" ''
+      function run_reaper() {
+        su - ${user} -c ${reaper}/bin/reaper &
+        export reaper_pid=$!
+        sleep 5
+        error_window=$(${xdotool}/bin/xdotool search --name 'error')
+        if [[ -n "$error_window" ]]; then
+            echo "found error window with ID: $error_window"
+            exit 1
+        fi
+        kill $reaper_pid
+      }
+
+      ${xvfb-run}/bin/xvfb-run -a sh -c "$(declare -f run_reaper); run_reaper"
+    '';
+in
+{
+  machine = { config, pkgs, ... }:
+    let
+      user = "ferris";
+    in
+    {
+      inherit imports;
+
+      # REAPER is unfree software
+      nixpkgs.config.allowUnfree = true;
+
+      users.users = {
+        "${user}" = {
+          isNormalUser = true;
+          description = user;
+          home = "/home/${user}";
+          createHome = true;
+        };
+        # Our scripts
+        root = {
+          hashedPassword = "";
+          hashedPasswordFile = null;
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        reaper
+      ] ++ [
+        (mkReaperDryRun {
+          inherit user;
+          inherit (pkgs) reaper xdotool xvfb-run;
+        })
+      ];
+    };
+
+  testCargoReaperNew = ''
+    machine.start()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("network.target")
+    machine.succeed("cargo reaper new reaper_test")
+  '';
+}
