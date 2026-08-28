87347dc50a688e169ebfaf67fb562bedb45b8f59
eureka-cpu <eureka-cpu@users.noreply.github.com>
2026-08-28 21:35:54 +0000

    deploy: bc5251ac3da33127850e4f69fa61fd5021b58294

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-99cb1245.js',
+                path_to_root + 'searchindex-647707ff.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
