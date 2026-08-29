c9878d6c5634a7ee35c6ae6634196cb452c7f6f2
eureka-cpu <github.eureka@gmail.com>
2026-08-28 18:07:42 -0700

    Specify displays

--- nixos/tests/clean/default.nix
+++ nixos/tests/clean/default.nix
@@ -16,7 +16,7 @@ testers.runNixOSTest {
     corro.wait_for_unit("multi-user.target")
 
     # Launch REAPER once to initialize `~/.config/REAPER/UserPlugins`
-    corro.succeed("su - corro -c 'cargo-reaper run --no-build --headless --timeout 5s --stdout null --stderr null'", timeout=20)
+    corro.succeed("su - corro -c 'cargo-reaper run --no-build --headless --display :99 --timeout 5s --stdout null --stderr null'", timeout=20)
 
     corro.succeed("su - corro -c 'cargo-reaper link ${plugin}/lib/${plugin_name}.*'", timeout=15)
     corro.succeed("su - corro -c 'test -e ~/.config/REAPER/UserPlugins/${plugin_name}.*'", timeout=15)
